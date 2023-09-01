e5c0d4d76021ec6f5df5604a8c4dab9243740bb4
j22868706 <j22868706@gmail.com>
2023-09-01 00:14:59 -0500

    09010020

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import *
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
+import mysql.connector
+import json
+app.debug = True
+
+
 
 # Pages
 @app.route("/")
@@ -16,5 +21,58 @@ def booking():
 @app.route("/thankyou")
 def thankyou():
 	return render_template("thankyou.html")
+
+@app.route("/api/attractions")
+def attractions():
+
+    con = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root1234",
+        database="taipeiDayTrip"
+    )
+
+    cursor = con.cursor()
+    
+    params = []
+    keyword = request.args.get('keyword')
+    page = int(request.args.get('page', 0))
+
+  
+    if keyword:
+        query += " WHERE name LIKE %s OR mrt = %s"
+        params.append('%' + keyword + '%')
+        params.append(keyword)
+
+    query += " LIMIT %s, 12"
+    params.append(page * 12)
+
+    cursor.execute(query, params)
+
+    data = cursor.fetchall()
+
+    con.close()
+
+    response_data = {
+        "nextPage": page + 1,
+        "data": []
+    }
+
+    for row in data:
+        attraction = {
+            "id": row[0],
+            "name": row[2],
+            "category": row[3],
+            "description": row[4],
+            "address": row[5],
+            "transport": row[6],
+            "mrt": row[7],
+            "lat": row[8],
+            "lng": row[9],
+        }
+        response_data["data"].append(attraction)
+	
+    return json.dumps(response_data, ensure_ascii=False).encode('utf8')
+
 
 app.run(host="0.0.0.0", port=3000)
