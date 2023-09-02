506620f7fea567a2337d673accfeb01e4e468e79
j22868706 <j22868706@gmail.com>
2023-09-02 01:15:46 -0500

    09020115

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app.config["TEMPLATES_AUTO_RELOAD"]=True
 import mysql.connector
 import json
 app.debug = True
-
-
+from collections import OrderedDict
 
 # Pages
 @app.route("/")
@@ -22,57 +21,165 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
+
 @app.route("/api/attractions")
 def attractions():
+    try:
 
-    con = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root1234",
-        database="taipeiDayTrip"
-    )
+        con = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root1234",
+            database="taipeiDayTrip"
+        )
 
-    cursor = con.cursor()
-    
-    params = []
-    keyword = request.args.get('keyword')
-    page = int(request.args.get('page', 0))
+        cursor = con.cursor()
 
-  
-    if keyword:
-        query += " WHERE name LIKE %s OR mrt = %s"
-        params.append('%' + keyword + '%')
-        params.append(keyword)
+        attraction_list = []
+        keyword = request.args.get('keyword')
+        page = int(request.args.get('page', 0))
 
-    query += " LIMIT %s, 12"
-    params.append(page * 12)
+        if keyword:
+            query = "SELECT * FROM attractions WHERE (name LIKE %s COLLATE utf8mb4_bin) OR (mrt = %s COLLATE utf8mb4_bin)"
+            attraction_list.append('%' + keyword + '%')
+            attraction_list.append(keyword)
 
-    cursor.execute(query, params)
+        query += " ORDER BY id LIMIT %s, 12"
+        attraction_list.append(page * 12)
 
-    data = cursor.fetchall()
+        cursor.execute(query, attraction_list)
 
-    con.close()
+        data = cursor.fetchall()
 
-    response_data = {
-        "nextPage": page + 1,
-        "data": []
-    }
+        next_page_query = "SELECT COUNT(*) FROM attractions WHERE (name LIKE %s) OR (mrt = %s ) LIMIT %s, 12"
+        cursor.execute(next_page_query, attraction_list)
+        next_page_data = cursor.fetchall()
 
-    for row in data:
-        attraction = {
-            "id": row[0],
-            "name": row[2],
-            "category": row[3],
-            "description": row[4],
-            "address": row[5],
-            "transport": row[6],
-            "mrt": row[7],
-            "lat": row[8],
-            "lng": row[9],
+        total_results = next_page_data[0][0]  
+
+        response_data = OrderedDict()
+        response_data["nextPage"] = page + 1 if len(data) >= 12 else None  
+        response_data["data"] = []
+
+        for row in data:
+            attraction = OrderedDict()
+            attraction["id"] = row[0]
+            attraction["name"] = row[2]
+            attraction["category"] = row[3]
+            attraction["description"] = row[4]
+            attraction["address"] = row[5]
+            attraction["transport"] = row[6]
+            attraction["mrt"] = row[7]
+            attraction["lat"] = row[8]
+            attraction["lng"] = row[9]
+            attraction["images"] = []
+
+            img_query = "SELECT imageUrl FROM attractionImages WHERE attractionRownumber = %s"
+            cursor.execute(img_query, (row[1],))
+            img_data = cursor.fetchall()
+
+            attraction["images"] = [img[0] for img in img_data]
+
+            response_data["data"].append(attraction)
+
+        con.close()
+
+        return json.dumps(response_data, ensure_ascii=False).encode('utf8')
+
+    except Exception as e:
+        error_response = {
+            "error": True,
+            "message": "請按照情境提供對應的錯誤訊息"
         }
-        response_data["data"].append(attraction)
-	
-    return json.dumps(response_data, ensure_ascii=False).encode('utf8')
+        return json.dumps(error_response, ensure_ascii=False).encode('utf8'), 500 
 
+
+@app.route("/api/attraction/<int:attractionId>")
+def get_attraction(attractionId):
+    print(attractionId)
+    try:
+        con = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root1234",
+            database="taipeiDayTrip"
+        )
+
+        cursor = con.cursor()
+
+        query = "SELECT * FROM attractions WHERE id = %s"
+        cursor.execute(query, (attractionId,))
+        data = cursor.fetchall()
+
+        if len(data) == 0:
+            error_response = {
+                "error": True,
+                "message": "請按照情境提供對應的錯誤訊息"
+            }
+            return json.dumps(error_response, ensure_ascii=False).encode('utf8'), 400  
+        
+        attraction = {
+            "id": data[0][0],
+            "name": data[0][2],
+            "category": data[0][3],
+            "description": data[0][4],
+            "address": data[0][5],
+            "transport": data[0][6],
+            "mrt": data[0][7],
+            "lat": data[0][8],
+            "lng": data[0][9],
+            "images": []
+        }
+
+        img_query = "SELECT imageUrl FROM attractionImages WHERE attractionRownumber = %s"
+        cursor.execute(img_query, (data[0][1],))
+        img_data = cursor.fetchall()
+
+        attraction["images"] = [img[0] for img in img_data]
+
+        con.close()
+
+        return json.dumps(attraction, ensure_ascii=False).encode('utf8')
+
+    except Exception as e:
+        error_response = {
+            "error": True,
+            "message": "請按照情境提供對應的錯誤訊息"
+        }
+        return json.dumps(error_response, ensure_ascii=False).encode('utf8'), 500  
+
+@app.route("/api/mrts")
+def mrts():
+    try:    
+        con = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root1234",
+            database="taipeiDayTrip"
+        )
+
+        cursor = con.cursor()
+        mrts_list = []
+        mrts_response_data = {"data": mrts_list}
+
+        query = "SELECT DISTINCT mrt FROM attractions ORDER BY (SELECT COUNT(*) FROM attractions AS a WHERE a.mrt = attractions.mrt) DESC"
+        cursor.execute(query)
+        mrt_results = cursor.fetchall()
+
+        for mrt_result in mrt_results:
+            mrts_list.append(mrt_result[0])
+
+        cursor.close()
+        con.close() 
+
+        return json.dumps(mrts_response_data, ensure_ascii=False).encode('utf8')
+    
+    except Exception as e:
+        mrt_error_response = {
+            "error": True,
+            "message": "請按照情境提供對應的錯誤訊息"
+        }
+        return json.dumps(mrt_error_response, ensure_ascii=False).encode('utf8'), 500 
+          
 
 app.run(host="0.0.0.0", port=3000)
